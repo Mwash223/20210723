e569b6f49fde61232da119e01717dc24e5e45e04
Malik Washington <kingmalikwashington@gmail.com>
2021-07-23 13:17:41 -0500

    changed note

---
+++ script.js
@@ -0,0 +1,14 @@
+
+let saleIteams = [];
+let doneListing = false;
+
+while (doneListing == false){
+    let userItem = prompt("What would you like to sell? Type D when finished.").toLowerCase();
+    if (userItem == "d"){
+        break;
+    }
+    saleIteams.push(userIteam);
+    }
+   
+    console.log(saleIteams);
+   
